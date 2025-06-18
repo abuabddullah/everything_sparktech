0cc13ee3d664b7cfe03573bc04bdd0d8e2f4411c
asifaowadud <asifaowadud@gmail.com>
2025-06-18 13:58:01 +0600

    working on brand model

---
+++ src/app/modules/brand/brand.validation.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+const createBrandValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string()
+      .nonempty("Brand name is required")
+      .max(100, "Brand name should not exceed 100 characters"),
+    createdBy: z.string()
+  })
+});
+
+
+const updateBrandValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string()
+      .max(100, "Brand name should not exceed 100 characters")
+      .optional(),
+    createdBy: z.string().optional(),
+    isActive: z.boolean().optional()
+  })
+});
+
+export const brandValidation = {
+  createBrandValidationSchema,
+  updateBrandValidationSchema
+}
